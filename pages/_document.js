45c5e23e1108ac83b970be338041d4bef8f649a6
Fredika Benz <benedictbenz.kerja@gmail.com>
2024-02-21 00:44:08 +0700

    Feature tes new branch (#1)

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
+    // tambahkan ke branch baru 
     <Html lang="en">
       <Head />
       <body>
