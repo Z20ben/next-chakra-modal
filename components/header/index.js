d7a99f1161b673319bf9546b15c48ac27d27f382
Fredika Benz <benedictbenz.kerja@gmail.com>
2024-02-22 10:36:25 +0700

    add footer comp (#2)

---
+++ components/header/index.js
@@ -0,0 +1,8 @@
+import Menu from "../menu"
+import { withAuth } from "../with-auth"
+
+function Header() {
+  return <div><Menu /></div>
+}
+
+export default withAuth(Header)
---
+++ components/with-auth/index.js
@@ -0,0 +1,8 @@
+export function withAuth(Component) {
+  return function WithAuth(props) {
+    const isLogin = false;
+
+    if (!isLogin) return <div>Anda Harus Login</div>;
+    return <Component {...props} />
+  }
+}
