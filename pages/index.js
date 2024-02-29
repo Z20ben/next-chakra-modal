126014ae2b6ac774ec02bd6c8d9ed18f279d2573
Fredika Benz <benedictbenz.kerja@gmail.com>
2024-02-29 12:15:04 +0700

    Hari ke9 api/routes (#8)
    * add fetch lokal api in homepage
    
    * add external api in lokal api

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,17 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Content from "@/components/content"
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 export default function Main() {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((res) => console.log("response => ", res))
+
+      .catch((err) => console.log("error => ", err));
+  }, []);
+
   return (
     <div>
       <Layout metaTitle="Home" metaDescription="Semua informasi home" >
