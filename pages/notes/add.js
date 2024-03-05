330e57d3b5fc88c8af7afc1b051f3c56710c8b1d
Fredika Benz <benedictbenz.kerja@gmail.com>
2024-03-05 23:50:52 +0700

    Hari ke12 api crud (#11)
    * add chakra component & map data notes
    
    * add comp add notes with create post method
    
    * add chakraprovider
    
    * install chakra

---
+++ pages/notes/add.js
@@ -0,0 +1,70 @@
+import { Box, Button, Flex, Grid, GridItem, Card, Heading, Text, Textarea, Input } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+const LayoutComponent = dynamic(() => import("@/layout"))
+export default function AddNotes({ }) {
+  const router = useRouter();
+  const [notes, setNotes] = useState(
+    {
+      title: "",
+      description: "",
+    }
+  );
+
+  const handleSubmit = async () => {
+    try {
+      const res = await fetch('https://paace-f178cafcae7b.nevacloud.io/api/notes', {
+        method: 'POST', headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(notes)
+      })
+      const result = await res.json()
+      if (result?.success) {
+        router.push("/notes")
+      }
+    } catch (error) {
+
+    }
+  }
+
+  // console.log("notes =>", notes)
+  return (
+    <>
+      <LayoutComponent metaTitle="Notes" metaDescription="Semua informasi catatan" >
+        <Card margin='5' padding='5'>
+          <Heading>Add Notes</Heading>
+          <Grid gap='5'>
+            <GridItem>
+              <Text>Title</Text>
+              <Input type="text" onChange={(event) => setNotes({ ...notes, title: event.target.value })}></Input>
+            </GridItem>
+            <GridItem>
+              <Text >Description</Text>
+              <Textarea onChange={(event) => setNotes({ ...notes, description: event.target.value })} />
+            </GridItem>
+            <GridItem>
+              <Button flex='1' colorScheme='blue' onClick={() => handleSubmit()}>
+                Submit
+              </Button>
+            </GridItem>
+          </Grid>
+        </Card>
+        {/* {notes.data.map((item) => (
+          <div>
+            <Link href={`/notes/${item.id}`}>{item.title}</Link>
+          </div>
+        ))} */}
+      </LayoutComponent>
+
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://paace-f178cafcae7b.nevacloud.io/api/notes')
+  const notes = await res.json()
+  return { props: { notes }, revalidate: 10 }
+}
--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,17 +1,62 @@
+import { Box, Button, Flex, Grid, GridItem, Card, CardBody, CardHeader, CardFooter, Heading, Text } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
-import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 const LayoutComponent = dynamic(() => import("@/layout"))
-export default function Notes({ notes }) {
-  // console.log("data notes : ", notes)
+export default function Notes({ }) {
+  const router = useRouter();
+  const [notes, setNotes] = useState();
+  useEffect(() => {
+    async function fetchingData() {
+      const res = await fetch("https://paace-f178cafcae7b.nevacloud.io/api/notes")
+      const listNotes = await res.json();
+      setNotes(listNotes)
+    }
+    fetchingData();
+  }, []);
+
+  // console.log("notes =>", notes)
   return (
     <>
       <LayoutComponent metaTitle="Notes" metaDescription="Semua informasi catatan" >
-        {notes.data.map((item) => (
+        <Box padding='5'>
+          <Flex justifyContent='end'>
+            <Button colorScheme={'blue'} onClick={() => router.push("/notes/add")}>Add Notes</Button>
+          </Flex>
+          <Flex>
+            <Grid templateColumns='repeat(3, 1fr)' gap={5}>
+              {notes?.data?.map((item) => (
+                <GridItem>
+                  <Card>
+                    <CardHeader>
+                      <Heading>{item?.title}</Heading>
+                    </CardHeader>
+                    <CardBody>
+                      <Text>{item?.description}</Text>
+                    </CardBody>
+                    <CardFooter
+                      justify='space-between'
+                      flexWrap='wrap'
+                    >
+                      <Button flex='1' colorScheme='orange'>
+                        Edit
+                      </Button>
+                      <Button flex='1' colorScheme='red' >
+                        Delete
+                      </Button>
+                    </CardFooter>
+                  </Card>
+                </GridItem>
+              ))}
+            </Grid>
+          </Flex>
+        </Box>
+        {/* {notes.data.map((item) => (
           <div>
             <Link href={`/notes/${item.id}`}>{item.title}</Link>
           </div>
-        ))}
+        ))} */}
       </LayoutComponent>
 
     </>
